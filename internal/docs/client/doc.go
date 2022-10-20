edfc5c0181a197fecc90164376de8aaf596872ad
Junyoung Choi <ccjy1113@gmail.com>
2022-10-20 03:22:48 -0400

    🔧 Update custom template for client

--- internal/docs/client/doc.go
+++ internal/docs/client/doc.go
@@ -1,3 +1,3 @@
 package docs
 
-//go:generate oapi-codegen --config client.yaml spec.yaml
+//go:generate oapi-codegen -old-config-style -package=client -generate types,client -templates=. -o ../../../pkg/client/client.go spec.yaml
