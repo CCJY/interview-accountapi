2917ebade23041989e62c554f3942ed0766bc096
Junyoung Choi <ccjy1113@gmail.com>
2022-10-21 03:35:26 -0400

    Update go generate

--- internal/docs/client/doc.go
+++ internal/docs/client/doc.go
@@ -1,3 +1,3 @@
-package docs
+package client
 
-//go:generate oapi-codegen -old-config-style -package=client -generate types,client -templates=. -o ../../../pkg/client/client.go spec.yaml
+//go:generate oapi-codegen -old-config-style -package=client -generate types,client -templates=../templates -o ../../../pkg/client/client.go spec.yaml
