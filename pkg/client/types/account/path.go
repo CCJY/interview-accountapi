9cabb726d50e89f46e0a7d38b651b9065c4d403b
Junyoung Choi <ccjy1113@gmail.com>
2022-10-24 04:47:12 -0400

    Update unnecessary files

--- pkg/client/types/account/path.go
+++
@@ -1,8 +0,0 @@
-package account
-
-const (
-	OperationPathCreateAccount string = "/v1/organisation/accounts"
-	OperationPathAllAccount    string = "/v1/organisation/accounts"
-	OperationPathGetAccount    string = "/v1/organisation/accounts/{account_id}"
-	OperationPathDeleteAccount string = "/v1/organisation/accounts/{account_id}"
-)
