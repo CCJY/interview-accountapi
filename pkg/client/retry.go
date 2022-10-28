2a5b445963ee537be80a2cbd5027e7edd0d1e7bc
Junyoung Choi <ccjy1113@gmail.com>
2022-10-27 20:21:10 -0400

    ✅ Add retry tests

--- pkg/client/retry.go
+++ pkg/client/retry.go
@@ -2,6 +2,8 @@ package client
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"time"
 )
 
@@ -17,9 +19,9 @@ func Req(timeout int) bool {
 	return true
 }
 
-func (r *Retry) RetryRequest() (bool, bool) {
+func (r *Retry) TRetryRequest() (bool, bool) {
 	var result bool
-	ch := make(chan bool, r.RetryMax)
+	ch := make(chan bool, 1)
 	for i := 0; i < r.RetryMax; i++ {
 		fmt.Printf("retrying:%d\n", i)
 		fmt.Printf("Req timeout:%d\n", int(r.RetryInterval)-(int(r.RetryInterval)*i)/r.RetryMax)
@@ -34,4 +36,62 @@ func (r *Retry) RetryRequest() (bool, bool) {
 
 	}
 	return false, false
+}
+
+type RetryResult struct {
+	Response *http.Response
+	Error    error
+}
+
+type RetryHttpRequestFn func(*http.Request) (*http.Response, error)
+
+func (r *Retry) RetryRequest(client *http.Client, request *http.Request) *RetryResult {
+	var result *RetryResult
+	ch := make(chan *RetryResult, 1)
+
+	doFn := func(c *http.Client, req *http.Request) *RetryResult {
+		got, err := c.Do(req)
+		return &RetryResult{
+			Response: got,
+			Error:    err,
+		}
+	}
+
+	ch <- doFn(client, request)
+
+	// err "http: ContentLength=36 with Body length 0"
+	// https://github.com/golang/go/issues/19653
+
+	for retried := 1; retried <= r.RetryMax; retried++ {
+		fmt.Printf("Retried: %d", retried)
+		select {
+		case result = <-ch:
+			var isError bool
+			if http.StatusInternalServerError <= result.Response.StatusCode && result.Response.StatusCode <= 599 {
+				isError = true
+			}
+			if result.Error != nil {
+				isError = true
+			}
+
+			if !isError {
+				return result
+			}
+			// Close the previous response's body. But
+			// read at least some of the body so if it's
+			// small the underlying TCP connection will be
+			// re-used. No need to check for errors: if it
+			// fails, the Transport won't reuse it anyway.
+			const maxBodySize = 4 << 10
+
+			io.CopyN(io.Discard, result.Response.Body, maxBodySize)
+
+			defer result.Response.Body.Close()
+
+			ch <- doFn(client, request)
+		case <-time.After(time.Duration(r.RetryInterval) * time.Millisecond):
+		}
+	}
+
+	return result
 }
--- pkg/client/retry_test.go
+++ pkg/client/retry_test.go
@@ -1,11 +1,18 @@
 package client
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
 	"time"
 )
 
-func TestRetry_RetryRequest(t *testing.T) {
+func TestRetry_TRetryRequest(t *testing.T) {
 	type fields struct {
 		RetryInterval time.Duration
 		RetryMax      int
@@ -40,13 +47,153 @@ func TestRetry_RetryRequest(t *testing.T) {
 				RetryInterval: tt.fields.RetryInterval,
 				RetryMax:      tt.fields.RetryMax,
 			}
-			got, got1 := r.RetryRequest()
+			got, got1 := r.TRetryRequest()
 			if got != tt.want {
 				t.Errorf("Retry.RetryRequest() got = %v, want %v", got, tt.want)
 			}
 			if got1 != tt.want1 {
 				t.Errorf("Retry.RetryRequest() got1 = %v, want %v", got1, tt.want1)
 			}
+		})
+	}
+}
+
+type TestData struct {
+	Name    string `json:"name,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+type TestServerResponse struct {
+	Data         interface{} `json:"data,omitempty"`
+	ErrorMessage *string     `json:"error_message,omitempty"`
+}
+
+func TestRetry_RetryRequest(t *testing.T) {
+	type fields struct {
+		RetryInterval time.Duration
+		RetryMax      int
+	}
+
+	type args struct {
+		method string
+		data   interface{}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		argsFn func(string, string, io.Reader) *http.Request //method, url, io.Reader
+		args   *args
+		want   *TestServerResponse
+		want1  int //statusc ode
+	}{
+		// TODO: Add test cases.
+		{
+			fields: fields{
+				RetryInterval: 100,
+				RetryMax:      3,
+			},
+			args: &args{
+				method: "",
+				data: &TestData{
+					Name:    "Hello",
+					Message: "Message",
+				},
+			},
+			argsFn: func(method, url string, r io.Reader) *http.Request {
+				req, _ := http.NewRequest(method, url, r)
+
+				return req
+			},
+			want: &TestServerResponse{
+				Data: &TestData{
+					Name:    "Hello",
+					Message: "Message",
+				},
+				ErrorMessage: nil,
+			},
+			want1: 200,
+		},
+
+		{
+			fields: fields{
+				RetryInterval: 100,
+				RetryMax:      3,
+			},
+			args: &args{
+				method: "",
+				data:   nil,
+			},
+			argsFn: func(method, url string, r io.Reader) *http.Request {
+				req, _ := http.NewRequest(method, url, r)
+
+				return req
+			},
+			want:  nil,
+			want1: 200,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			retried := 0
+			s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				retried += 1
+				var result *TestServerResponse
+				if retried < tt.fields.RetryMax {
+					w.WriteHeader(500)
+					result = tt.want
+					dataBytes, _ := json.Marshal(result)
+
+					fmt.Fprintln(w, string(dataBytes))
+
+					return
+				}
+				w.WriteHeader(tt.want1)
+				result = tt.want
+				dataBytes, _ := json.Marshal(result)
+
+				fmt.Fprintln(w, string(dataBytes))
+			}))
+
+			defer s.Close()
+
+			c := &http.Client{}
+			r := &Retry{
+				RetryInterval: tt.fields.RetryInterval,
+				RetryMax:      tt.fields.RetryMax,
+			}
+			reader, _ := json.Marshal(tt.args.data)
+			request := tt.argsFn(tt.args.method, s.URL, bytes.NewReader(reader))
+			got := r.RetryRequest(c, request)
+
+			var responseData TestServerResponse
+			bodybytes, err := io.ReadAll(got.Response.Body)
+			if err != nil {
+				t.Errorf("%v", err)
+			}
+
+			defer got.Response.Body.Close()
+
+			err = json.Unmarshal(bodybytes, &responseData)
+			if err != nil {
+				t.Errorf("%v", err)
+			}
+
+			if got.Response.StatusCode != tt.want1 {
+				t.Errorf("Retry.RetryRequest() = %v, want1 %v", got.Response.StatusCode, tt.want1)
+			}
+
+			var wantData TestServerResponse
+			wantBytes, _ := json.Marshal(tt.want)
+			err = json.Unmarshal(wantBytes, &wantData)
+			if err != nil {
+				t.Errorf("%v", err)
+			}
+
+			if !reflect.DeepEqual(responseData, wantData) {
+				responseDataBytes, _ := json.Marshal(responseData)
+				wantBytes, _ := json.Marshal(tt.want)
+				t.Errorf("Retry.RetryRequest() = %v, want %v", string(responseDataBytes), string(wantBytes))
+			}
+
 		})
 	}
 }
