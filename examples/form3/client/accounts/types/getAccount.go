0f4af5c39f6fdc4ab116a64541c7dc8114e7f9d9
Junyoung Choi <ccjy1113@gmail.com>
2022-10-28 21:59:01 -0400

    Feat GetAllAccount With Filters

--- examples/form3/client/accounts/types/getAccount.go
+++ examples/form3/client/accounts/types/getAccount.go
@@ -6,31 +6,6 @@ import (
 	"github.com/ccjy/interview-accountapi/pkg/client"
 )
 
-// PageFilter defines model for PageFilter.
-type FilterPage struct {
-	// Page number being requested, defaults to 0.
-	Number int
-
-	// Size of the page being requested, defaults to 100.
-	Size int
-}
-
-type FilterKey string
-
-const (
-	FilterAccountNumber FilterKey = "account_number"
-	FilterBankId        FilterKey = "bank_id"
-	FilterBankIdCode    FilterKey = "bank_id_code"
-	FilterCountry       FilterKey = "country"
-	FilterCustomerId    FilterKey = "customer_id"
-	FilterIban          FilterKey = "iban"
-)
-
-type Filter struct {
-	Key   FilterKey
-	Value string
-}
-
 type GetAccountResponse = commons.ResponseData[models.AccountData]
 
 type GetAccountRequestContext = client.RequestContext[GetAccountResponse]
